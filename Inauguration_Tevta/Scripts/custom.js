4a23539e73a3db8d9a86b37a614a2eb7acf436ec
Naveed Shahzad <lhrciit@gmail.com>
2023-05-18 13:36:28 +0500

    first commit

---
+++ Inauguration_Tevta/Scripts/custom.js
@@ -0,0 +1,24 @@
+﻿
+     
+function ShowC(heading, message, isSuccess) {
+    $('#cpModal').modal('toggle');
+    $('#cpModal').modal('show');
+    var pHeading = document.getElementById('rpHeader');
+    pHeading.innerHTML = heading + '<b> </b>';
+
+    var element = document.getElementById('rpBody');
+    while (element.hasChildNodes()) {
+        element.removeChild(element.firstChild);
+    }
+    element.innerHTML = message;
+    document.getElementById('mFooter').style.display='none';
+   
+
+}
+function doMagic() {
+   
+    $('#cpModal').modal('toggle');
+    $('#cpModal').modal('show');
+    var div1 = document.getElementById('rpHeader');
+    div1.innerHTML = 'Change Password' + '<b> </b>';
+}
---
+++ Inauguration_Tevta/js/common.js
@@ -0,0 +1,28 @@
+﻿$(function() {
+    $('.numbers-only').on("keypress", function(event) {
+        return isNumeric(event);
+    });
+
+});
+function days_between(firstDate, secondDate) {
+    // The number of milliseconds in one day
+    var oneDay = 1000 * 60 * 60 * 24;
+    // Convert both dates to milliseconds
+    // Calculate the difference in milliseconds
+    var differenceInMs = Math.abs(firstDate.getTime() - secondDate.getTime());
+    // Convert back to days and return
+    return Math.round(differenceInMs / oneDay);
+}
+function addCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+function isNumeric(evt) {
+
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    console.log(charCode);
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
